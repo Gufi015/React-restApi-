65951bee851e4d7bdcbc035dab9d637b80892824
Rufino Rojas <lucras015@hotmail.com>
2019-02-11 13:02:46 -0600

    consumo de api para React Js

--- src/App.js
+++ src/App.js
@@ -1,27 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const User = (props) => (
+  <ul>
+    <li>Usuarios: {props.name}</li>
+    <li>Password: {props.email}</li>
+  </ul>
+)
+
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+  constructor(props){
+    super(props)
+    this.state = {
+      users:[]
+    }
+  }
+  componentWillMount(){
+    fetch('https://randomuser.me/api/?results=500')
+    .then(response => response.json())
+    .then(users =>{
+      users.results.forEach(user =>{
+        let data = {
+          name:user.name.first,
+          email:user.email,
+          password:user.login.password
+        }
+        console.log('aqui van los datos de los usuarios del rest api ' + data)
+        this.setState({users:this.state.users.concat([data])})
+      })
+    })
+  }
+
+  render(){
+    console.log(this.state.users.length)
+
+    if(this.state.users.length > 0){
+      return(
+        <div>
+          {this.state.users.map(user => <User key={user.password} name={user.name} email={user.email}/>)}
+        </div>
+      )
+    }
+    return(
+      <p>Cargando Usuarios...</p>
+    )
   }
 }
 
